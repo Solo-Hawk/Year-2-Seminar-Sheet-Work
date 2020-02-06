0f3ddd35ac1473d9be483c2266a114acd09331b1
Solo-Hawk <Solo-Hawk@users.noreply.github.com>
2020-02-06 19:53:53 +0000

    Sheet 7 Practical Finished

---
+++ Sheet 7/ProceduralGeneration/Source/ProceduralGeneration/ProceduralGenerationGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProceduralGenerationGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROCEDURALGENERATION_API AProceduralGenerationGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
